fec2e8e27bb42dae29050d0e68239c3d2eae546e
Andrew Dutson <Dutsandrew@gmail.com>
2023-03-15 22:01:30 -0600

    messages are sorted by recency

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,15 +1,23 @@
 const async = require('async'),
       Message = require('../models/message'),
-      he = require('he');
+      he = require('he'),
+      { DateTime } = require('luxon');
 
 exports.index = async (req, res, next) => {
   try {
     const messages = await Message.find();
+
     // remove escaped characters like ' or " used in sentences.
     messages.forEach((message) => {
       he.decode(message.title);
       he.decode(message.body);
     });
+
+    // sort to most recent
+    messages.sort((a, b) => {
+      return DateTime.fromISO(b.timestamp).diff(DateTime.fromISO(a.timestamp)).as('milliseconds');
+    });
+
     res.render("index", {
       title: "Members Only Chat",
       welcome: "Check out the latest messages:",
